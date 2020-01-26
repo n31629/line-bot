470ca956bd63958da88b4b1d619ebbfb68b6856c
Sean Tseng <n31629@gmail.com>
2020-01-27 00:21:42 +0800

    second commit

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def handle_message(event):
 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text='小胖是正妹'))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
